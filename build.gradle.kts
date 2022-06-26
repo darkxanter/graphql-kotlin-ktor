e04d632b0bbf341cd522aaaa44afa342d0668542
Sergey Shumov <sergey0001@gmail.com>
2022-06-26 18:17:10 +0300

    fix build

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,12 +11,14 @@ allprojects {
 println("version $version")
 
 nexusPublishing {
-    repositories {
-        sonatype {
-            nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
-            snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
-            username.set(properties["mavenCentralUser"] as String)
-            password.set(properties["mavenCentralPassword"] as String)
+    if (properties.containsKey("mavenCentralUser")) {
+        repositories {
+            sonatype {
+                nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
+                snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
+                username.set(properties["mavenCentralUser"] as String)
+                password.set(properties["mavenCentralPassword"] as String)
+            }
         }
     }
 }
